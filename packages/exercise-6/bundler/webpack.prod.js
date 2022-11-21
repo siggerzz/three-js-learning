1ff0734c6336f2813ffcac7376b27944630c1491
Ben Siggery <bensiggery28@gmail.com>
2022-11-21 16:00:44 +0000

    chore: add exercise 6 starter files

---
+++ packages/exercise-6/bundler/webpack.prod.js
@@ -0,0 +1,8 @@
+const { merge } = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const commonConfiguration = require('./webpack.common');
+
+module.exports = merge(commonConfiguration, {
+  mode: 'production',
+  plugins: [new CleanWebpackPlugin()],
+});
