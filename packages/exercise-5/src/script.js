378fb084ff9a31b2a44609c13e432588ab655967
Ben Siggery <bensiggery28@gmail.com>
2022-11-21 15:55:03 +0000

    feat: finish exercise 5

--- packages/exercise-5/src/script.js
+++ packages/exercise-5/src/script.js
@@ -7,18 +7,44 @@ const canvas = document.querySelector('canvas.webgl');
 // Scene
 const scene = new THREE.Scene();
 
+// Axes Helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
 
-mesh.position.set(0.7, -0.6, 1);
+const group = new THREE.Group();
+scene.add(group);
+group.position.y = 1;
+group.scale.y = 2;
+group.rotation.y = 1;
 
-mesh.scale.set(2, 0.5, 0.5);
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x888080 }),
+);
 
-scene.add(mesh);
+cube1.position.x = -2;
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff4938 }),
+);
+
+cube2.position.x = 0;
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xcc6694 }),
+);
+
+cube3.position.x = 2;
+
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
 
 /**
  * Sizes
@@ -32,7 +58,7 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.set(1, 1, 3);
+camera.position.set(0, 0, 3);
 scene.add(camera);
 
 /**
